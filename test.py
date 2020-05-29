3f53adb423ed34621948a88571c979085abafdf0
Mohd <mailme.suhail48@gmail.com>
2020-05-30 01:03:57 +0530

    first

---
+++ test.py
@@ -0,0 +1,45 @@
+import numpy as np
+import mnist
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+from keras.utils import to_categorical
+
+train_images = mnist.train_images()
+train_labels = mnist.train_labels()
+test_images = mnist.test_images()
+test_labels = mnist.test_labels()
+
+# Normalize the images.
+train_images = (train_images / 255) - 0.5
+test_images = (test_images / 255) - 0.5
+
+# Reshape the images.
+train_images = np.expand_dims(train_images, axis=3)
+test_images = np.expand_dims(test_images, axis=3)
+
+num_filters = 8
+filter_size = 3
+pool_size = 2
+
+# Build the model.
+model = Sequential([
+  Conv2D(num_filters, filter_size, input_shape=(28, 28, 1)),
+  MaxPooling2D(pool_size=pool_size),
+  Flatten(),
+  Dense(10, activation='softmax'),
+])
+
+# Compile the model.
+model.compile(
+  'adam',
+  loss='categorical_crossentropy',
+  metrics=['accuracy'],
+)
+
+# Train the model.
+model.fit(
+  train_images,
+  to_categorical(train_labels),
+  epochs=3,
+  validation_data=(test_images, to_categorical(test_labels)),
+)
